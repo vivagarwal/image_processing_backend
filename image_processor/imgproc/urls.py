fa861662f2a62059e5a1d6ec6dc09e5b3459f122
vivagarwal <vivek.jsr.topper@gmail.com>
2025-01-26 04:00:27 +0530

    added helloworld api and changed allowedhost * handling - removed commented url view

--- image_processor/imgproc/urls.py
+++ image_processor/imgproc/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UploadCSV,hello
 
 urlpatterns = [
-    # path('upload', UploadCSV.as_view(), name='upload_csv'),
+    path('upload', UploadCSV.as_view(), name='upload_csv'),
     path('',hello,name="hello world")
 ]
