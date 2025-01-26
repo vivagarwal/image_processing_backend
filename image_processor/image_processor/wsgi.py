7827e7d739875a5dd0381eb65735d31d619fffed
vivagarwal <vivek.jsr.topper@gmail.com>
2025-01-26 14:21:30 +0530

    still not able to see db connection in log hence adding logging in wsgi.py

--- image_processor/image_processor/wsgi.py
+++ image_processor/image_processor/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_processor.settings')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+logger.info(f"Using database: {os.getenv('DATABASE_URL')}")
+logger.info(f"Environment: {os.getenv('ENV')}")
 
 application = get_wsgi_application()
